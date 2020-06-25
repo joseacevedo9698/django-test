617775239e122e844868898701e98d387f18cd65
jose acevedo <jose.acevedo1@uac.edu.co>
2020-06-25 10:47:58 -0500

    dinamyc template

--- lolsito/views.py
+++ lolsito/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Summoner
 # Create your views here.
 def summoner_list(request):
-    return render(request, 'lolsito/summoner_list.html', {})
+    summoner = Summoner.objects.order_by('-created_date')
+    return render(request, 'lolsito/summoner_list.html', {'summoners':summoner})
